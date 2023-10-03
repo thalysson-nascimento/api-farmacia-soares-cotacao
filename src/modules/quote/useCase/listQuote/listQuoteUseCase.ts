4f0a2b99231e46f7a3177982e98d4e8f388926d9
Thalysson <thalyssonwg.nascimento@gmail.com>
2023-10-03 11:41:03 -0300

    feat: incluido camada de registro de listacao de cotas

--- src/modules/quote/useCase/listQuote/listQuoteUseCase.ts
+++ src/modules/quote/useCase/listQuote/listQuoteUseCase.ts
@@ -2,7 +2,14 @@ import { prismaCliente } from "../../../../database/prismaCliente";
 
 export class ListQuoteUseCase {
   async execute() {
-    const listQuote = await prismaCliente.quote.findMany();
+    const listQuote = await prismaCliente.quote.findMany({
+      select: {
+        id: true,
+        status: true,
+        hashQuote: true,
+        createdAt: true,
+      },
+    });
     return listQuote;
   }
 }
